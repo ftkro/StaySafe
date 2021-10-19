26462b1144f43aca72311561a3b1d0603084e45e
Ruairi Blake <76410753+Blakerc@users.noreply.github.com>
2021-10-19 14:06:45 -0400

    sets up controller and application

--- src/main/java/com/ent/staySafe/staySafeController.java
+++ src/main/java/com/ent/staySafe/staySafeController.java
@@ -1,5 +1,12 @@
 package com.ent.staySafe;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/checkin")
 public class staySafeController {
 
 }
