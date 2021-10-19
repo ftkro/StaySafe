b77d2fec86db38563df49757619502c73c2e2d05
Ruairi Blake <76410753+Blakerc@users.noreply.github.com>
2021-10-19 14:07:36 -0400

    sets up controller and application (#2)

---
+++ src/main/java/com/ent/staySafe/StaySafeApplication.java
@@ -0,0 +1,16 @@
+package com.ent.staySafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.ent.staySafe.StaySafeApplication;
+
+@SpringBootApplication
+public class StaySafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaySafeApplication.class, args);
+	}
+
+	
+}
