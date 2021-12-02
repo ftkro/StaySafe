255d796b376637b950faf9a894cb3143c1a71af6
Ruairi Blake <76410753+Blakerc@users.noreply.github.com>
2021-12-02 12:03:58 -0500

    save route

---
+++ src/main/java/com/ent/staySafe/service/StaySafeServiceImpl.java
@@ -0,0 +1,23 @@
+package com.ent.staySafe.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ent.staySafe.dao.StaySafeRepository;
+import com.ent.staySafe.entity.Stay;
+
+@Service
+public class StaySafeServiceImpl {
+	
+	private StaySafeRepository staySafeRepository;
+	
+	@Autowired
+	public StaySafeServiceImpl (StaySafeRepository theStaySafeRepository) {
+		staySafeRepository = theStaySafeRepository;
+	}
+
+	public void save(Stay theStay) {
+		staySafeRepository.save(theStay);
+
+	}
+}
